986b4cfa09fe27a3b0c7f472653169cdb2104cd2
ElizavetaNosova <58087973+ElizavetaNosova@users.noreply.github.com>
2020-12-06 15:33:54 +0300

    [fix] format of spelling data

--- api.py
+++ api.py
@@ -100,7 +100,7 @@ def correct_spelling():
     # corrected_text = spelling.make_changes(text, spelling_problems)
     # with open(os.path.join(app.config['UPLOAD_FOLDER'], new_file_name), encoding='utf-8') as f:
     #     f.write(corrected_text)
-    print(request.form)
+    print(request.json)
     return json.dumps({'success':True}), 200
     
 
