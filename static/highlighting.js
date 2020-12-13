ddd9b1dd8b9cad2cf2976834e9473b14aea38311
Pavel Tugolukov <pavel@freighthub.com>
2020-12-13 01:22:38 +0100

    fix: highlighting change unnecessary console.logs into console.debugs

--- static/highlighting.js
+++ static/highlighting.js
@@ -7,39 +7,43 @@ function addTags(text, labels) {
 
 function transformProblemsIntoDictionary(problems) {
     const id2labels = {};
-    Object.keys(problems).forEach(function(problemType){
+    const problemTypes = Object.keys(problems)
+    console.debug("transformProblemsIntoDictionary@problemTypes:", problemTypes)
+    problemTypes.forEach(function(problemType){
         const similarProblems = problems[problemType]
 
         similarProblems
             .filter(problem => {
-                const isBosNumber = typeof problem['bos'] === 'number';
+                const isBosNumber = typeof problem.bos === 'number';
                 if (!isBosNumber) console.error('bos is not a number', problem);
 
-                const isEndNumber = typeof problem['end'] === 'number';
+                const isEndNumber = typeof problem.end === 'number';
                 if (!isEndNumber) console.error('end is not a number', problem);
 
-                const isEndLargerThenBos = problem['end'] > problem['bos'];
+                const isEndLargerThenBos = problem.end > problem.bos;
                 if (!isEndLargerThenBos) console.error('end is not larger than bos', problem);
 
                 const isValid = isBosNumber && isEndNumber && isEndLargerThenBos;
 
                 return isValid
             })
-            .forEach(function(problem) {
-                for (let symbol_id = problem['bos']; symbol_id < symbol_id['end']; symbol_id++) {
-                    if (!id2labels[symbol_id]) {
-                        id2labels[symbol_id] = new Set();
+            .forEach(problem => {
+                console.debug("transformProblemsIntoDictionary > problem:", problem)
+                for (let symbolIndex = problem.bos; symbolIndex < problem.end; symbolIndex++) {
+                    if (!id2labels[symbolIndex]) {
+                        id2labels[symbolIndex] = new Set();
                         //check 2 similar tags case
                     }
-                    id2labels[symbol_id].add(problemType);
+                    id2labels[symbolIndex].add(problemType);
                 }
             })
     })
+    console.debug("transformProblemsIntoDictionary@id2labels:", id2labels)
     return id2labels
 }
 
 function areSetsEqual(set1, set2) {
-    console.log('areSetsEqual', set1,set2);
+    console.debug('areSetsEqual', set1, set2);
     if (!set1 && !set2) return true;
     if (typeof set1 !== typeof set2) return false;
     if (set1.size !== set2.size) return false;
@@ -49,7 +53,8 @@ function areSetsEqual(set1, set2) {
 }
 
 function highlightText(text, problems){
-    console.log('highlightText','@text', text, '@problems', problems);
+    console.debug('highlightText@text:', text);
+    console.debug('highlightText@problems:',problems)
     if (!text) {
         return '';
     }
@@ -63,8 +68,8 @@ function highlightText(text, problems){
             const currentTextFragment = text.slice(currentStartId, currentEndId);
 
             const currentHtmlPart = addTags(currentTextFragment, currentClassLabels);
-            console.log('currentHtmlPart: ', currentHtmlPart);
-            console.log('currentClassLabels: ', currentClassLabels)
+            console.debug('currentHtmlPart:', currentHtmlPart);
+            console.debug('currentClassLabels:', currentClassLabels)
             htmlParts2Highlight.push(currentHtmlPart);
             currentStartId = currentEndId;
             currentClassLabels = id2labels[currentEndId];
@@ -72,5 +77,6 @@ function highlightText(text, problems){
     }
     const lastTextFragment = text.slice(currentStartId);
     htmlParts2Highlight.push(addTags(lastTextFragment, currentClassLabels));
+
     return htmlParts2Highlight.join('');
 }
